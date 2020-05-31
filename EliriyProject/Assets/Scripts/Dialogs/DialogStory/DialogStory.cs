1e42c4f213969910f4b138ce8c8f5893411913a8
disablak <disablaksteam@gmail.com>
2020-05-31 22:44:28 +0300

    update

---
+++ EliriyProject/Assets/Scripts/Dialogs/DialogStory/DialogStory.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using Ink.Runtime;
+using TMPro;
+using UnityEngine;
+
+
+public class DialogStory : MonoBehaviour
+{
+    [SerializeField] private TextAsset       test_text_asset = null;
+    [SerializeField] private TextMeshProUGUI txt_main        = null;
+
+    [SerializeField] private Transform      root_answers     = null;
+    [SerializeField] private UIAnswerButton ui_answer_button = null;
+
+
+    private Story test_story = null;
+
+    private void Awake()
+    {
+        test_story = new Story(test_text_asset.text);
+        loadStoryText();
+    }
+
+    private void loadStoryText()
+    {
+        if (test_story.canContinue)
+        {
+            txt_main.text = test_story.ContinueMaximally();
+        }
+
+        spawnAnswers();
+    }
+
+    private void spawnAnswers()
+    {
+        List<Choice>         choices        = test_story.currentChoices;
+        List<UIAnswerButton> answer_buttons = new List<UIAnswerButton>();
+
+        if ( choices.Count == 0 )
+            closeDialog();
+        
+        for (var i = 0; i < choices.Count; i++)
+        {
+            Choice choice = choices[i];
+            int    index  = i;
+
+            UIAnswerButton answer_button = Instantiate(ui_answer_button, root_answers);
+            answer_button.init(choice.text, makeAnswer);
+
+            answer_buttons.Add(answer_button);
+
+            void makeAnswer()
+            {
+                test_story.ChooseChoiceIndex(index);
+                loadStoryText();
+
+                //Destroy buttons
+                foreach (var btn in answer_buttons)
+                    Destroy(btn.gameObject);
+            }
+        }
+    }
+
+    private void closeDialog()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ EliriyProject/Assets/Scripts/Dialogs/DialogStory/UIAnswerButton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAnswerButton : MonoBehaviour
+{
+    [SerializeField] private Button          button     = null;
+    [SerializeField] private TextMeshProUGUI txt_answer = null;
+
+    
+    public void init( string text, Action callback )
+    {
+        if (callback == null)
+        {
+            Debug.LogError( $"callback is null!" );
+            return;
+        }
+        
+        button.onClick.AddListener( () => callback.Invoke() );
+        
+        txt_answer.text = text;
+    }
+}
